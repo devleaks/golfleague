0f899c14d1320c6bac7eef85a13751095e57b2f0
Pierre M <devleaks.be@gmail.com>
2015-09-14 18:50:25 +0200

    back to old recur input (that works)

--- backend/modules/admin/views/competition/_form-recurinput.php
+++ backend/modules/admin/views/competition/_form-recurinput.php
@@ -3,6 +3,8 @@
 use common\models\Rule;
 use common\models\Course;
 
+use devleaks\recurinput\RecurInput;
+
 use kartik\widgets\DateTimePicker;
 use kartik\form\ActiveForm;
 
@@ -10,6 +12,7 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\bootstrap\Alert;
 
+$model->recurrence = 'RRULE:FREQ=WEEKLY;INTERVAL=2;BYDAY=TU;UNTIL=20150929T000000Z';
 /* @var $this yii\web\View */
 /* @var $model common\models\Competition */
 /* @var $form yii\widgets\ActiveForm */
@@ -55,51 +58,29 @@ use yii\bootstrap\Alert;
 
 	<?php if($model->competition_type == $model::TYPE_ROUND): ?>
 
-    <?= $form->field($model, 'course_id')->dropDownList(Course::getCourseList(true)) ?>
+	    <?= $form->field($model, 'course_id')->dropDownList(Course::getCourseList(true)) ?>
 
-	<?= $form->field($model, 'holes')->dropDownList(array(18 => '18', 9 => '9')) ?>
+		<?= $form->field($model, 'holes')->dropDownList(array(18 => '18', 9 => '9')) ?>
 
-	<div class='row'>
-		<div class='col-lg-4'>
-	    <?= $form->field($model, 'start_date')->widget(DateTimePicker::classname(), [
-	            'pluginOptions' => [
-	                'format' => 'yyyy-mm-dd hh:ii:ss',
-	                'todayHighlight' => true
-				]
-	    ])->label(Yii::t('golf', 'Start Date &amp; Time')) ?>
+		<div class='row'>
+			<div class='col-lg-4'>
+		    <?= $form->field($model, 'start_date')->widget(DateTimePicker::classname(), [
+		            'pluginOptions' => [
+		                'format' => 'yyyy-mm-dd hh:ii:ss',
+		                'todayHighlight' => true
+					]
+		    ])->label(Yii::t('golf', 'Start Date &amp; Time')) ?>
+			</div>
+
+			<div class='col-lg-8'>
+			<?= $form->field($model, 'recurrence')->widget(RecurInput::className(), [
+		            'pluginOptions' => [
+		                'todayHighlight' => true
+					]
+				])->label(Yii::t('golf', 'Repeat Event')) ?>
+			</div>
 		</div>
 
-		<div class='col-lg-8'>
-		<?php
-			echo $form->field($model, 'recurrence_text', [
-			    'addon' => [
-					'prepend' => [
-						'content' => Html::button('<span class="glyphicon glyphicon-repeat"></span>', [
-							'class'=>'btn btn-default',
-							'data'=> ['toggle' => 'modal', 'target' => '#recurrence-modal'],
-							'style' => 'background-color: #eee;' // !!
-						])
-						. PHP_EOL .
-						Html::button('<span class="glyphicon glyphicon-remove"></span>', [
-							'class'=>'btn btn-default remove-recurrence',
-							'style' => 'background-color: #eee;' // !!
-						]),
-						'asButton' => true,
-					],
-				]
-			])->label(Yii::t('golf', 'Repeat Event'));
-
-			// echo Html::activeHiddenInput($model, 'recurrence');
-			echo $form->field($model, 'recurrence')->textInput(['readonly' => true])->label(false);
-		
-            echo $this->render('scheduler', [
-				'form'  => $form,
-			]);
-		?>
-		</div>
-	</div>
-
-	<div class="clearfix"></div>
 	<?php endif; ?>
 
     <?= $form->field($model, 'status')->dropDownList($model::getLocalizedConstants('STATUS_')) ?>
